a2d2374268c64e13e5400c060476c6e3c80d9b40
m <scheer@freescience.de>
2018-12-09 11:03:39 +0100

    h.add_list_name_as_column removed

--- R/funs.R
+++ R/funs.R
@@ -153,14 +153,6 @@ h.y_as_first_col <- function(df) {
   df[, c("y", setdiff(names(df), "y"))]
 }
 
-h.add_list_name_as_column <- function(list_of_df){
-  lapply(names(list_of_df), function(N){
-    list_of_df[[N]]$df_name <- N
-    list_of_df[[N]]
-  }) %>% 
-    dplyr::bind_rows()
-}
-
 h.bind_rows_with_id <- function(...) dplyr::bind_rows(..., .id = "target")
 
 h.pj <- function(rcp) recipes::juice(recipes::prep(rcp, retain = TRUE))
--- make.R
+++ make.R
@@ -12,12 +12,15 @@ if (!dir.exists("reports")) {
 
 sub_plans <- plans#[1:3]
 flog.info(glue::glue("bind plans: {paste(sort(names(sub_plans)), collapse = ', ')}"))
-plan <- h.add_list_name_as_column(sub_plans) %>% 
+plan <- 
+  sub_plans %>% 
+  dplyr::bind_rows(.id = "df_name") %>% 
   dplyr::arrange(df_name)
 
 sub_plan <- 
   plan %>% 
-  h.minimal_plan(c("m_svmrbf_bayes_opt2", "m_rf_fr_pure"))
+#  h.minimal_plan(c("m_svmrbf_bayes_opt2", "m_rf_fr_pure")) %>% 
+  identity()
 
 config <- drake_config(sub_plan, cache_log_file = "cache_log.txt")
 vis_drake_graph(config, targets_only = TRUE) %>% print
