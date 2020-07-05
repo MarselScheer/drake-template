d31418aec9becde6836a8ff994c53ef2597a164e
Marsel Scheer <scheer@freescience.de>
2020-07-05 20:33:45 +0000

    add missing libraries for visualizing a drake-graph

--- R/libs.R
+++ R/libs.R
@@ -1,6 +1,8 @@
 # libraries necessary for the template
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(drake))
+suppressPackageStartupMessages(library(visNetwork))
+suppressPackageStartupMessages(library(lubridate))
 suppressPackageStartupMessages(library(logger))
 suppressPackageStartupMessages(library(glue))
 suppressPackageStartupMessages(library(RPushbullet))
