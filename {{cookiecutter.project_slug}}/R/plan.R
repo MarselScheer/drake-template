26d8a3b22f17e79cb19385506b2a7853bcb5e664
Marsel Scheer <scheer@freescience.de>
2021-04-23 10:08:51 +0200

    separate plans are sourced in plan.R

--- {{cookiecutter.project_slug}}/R/plan.R
+++ {{cookiecutter.project_slug}}/R/plan.R
@@ -1,25 +1,6 @@
 plans <- list()
 
-plans[["gen_data"]] <- drake::drake_plan(
-  my_data = data.frame(
-    a = rnorm(n = 100),
-    b = rnorm(n = 100)
-  )
-)
-
-plans[["model"]] <- drake::drake_plan(
-  my_model = lm(formula = b ~ a, data = my_data),
-  report =  drake::target(
-    command = {
-      rmarkdown::render(
-        input = drake::knitr_in("reports/presentation_template.Rmd"),
-        quiet = TRUE)
-      # file_out is necessary to rebuild the target if the html
-      # was modified/deleted
-      drake::file_out("reports/presentation_template.html")
-    })
-
-)
+source("R/plan_example.R")
 
 logger::log_info(glue::glue("bind plans: {paste(sort(names(plans)), collapse = ', ')}"))
 plan <-
